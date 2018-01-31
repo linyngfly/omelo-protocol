49306949643daf980045a68a61908645bfdb4a70
linyang <119745743@qq.com>
2018-01-31 10:22:49 +0800

    var 2 let

--- test/protocol.js
+++ test/protocol.js
@@ -1,13 +1,13 @@
-var should = require('should');
-var Protocol = require('../');
-var Package = Protocol.Package;
-var Message = Protocol.Message;
+let should = require('should');
+let Protocol = require('../');
+let Package = Protocol.Package;
+let Message = Protocol.Message;
 
 describe('Pomelo protocol test', function() {
   describe('String encode and decode', function() {
     it('should be ok to encode and decode Chinese string', function() {
-      var str = '你好, abc~~~';
-      var buf = Protocol.strencode(str);
+      let str = '你好, abc~~~';
+      let buf = Protocol.strencode(str);
       should.exist(buf);
       str.should.equal(Protocol.strdecode(buf));
     });
@@ -15,10 +15,10 @@ describe('Pomelo protocol test', function() {
 
   describe('Package encode and decode', function() {
     it('should keep the same data after encoding and decoding', function() {
-      var msg = 'hello world~';
-      var buf = Package.encode(Package.TYPE_DATA, Protocol.strencode(msg));
+      let msg = 'hello world~';
+      let buf = Package.encode(Package.TYPE_DATA, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Package.decode(buf);
+      let res = Package.decode(buf);
       should.exist(res);
       Package.TYPE_DATA.should.equal(res.type);
       should.exist(res.body);
@@ -26,9 +26,9 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should ok when encoding and decoding package without body', function() {
-      var buf = Package.encode(Package.TYPE_HANDSHAKE);
+      let buf = Package.encode(Package.TYPE_HANDSHAKE);
       should.exist(buf);
-      var res = Package.decode(buf);
+      let res = Package.decode(buf);
       should.exist(res);
       Package.TYPE_HANDSHAKE.should.equal(res.type);
       should.not.exist(res.body);
@@ -37,14 +37,14 @@ describe('Pomelo protocol test', function() {
 
   describe('Message encode and decode', function() {
     it('should be ok for encoding and decoding request', function() {
-      var id = 128;
-      var compress = 0;
-      var route = 'connector.entryHandler.entry';
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_REQUEST, compress,
+      let id = 128;
+      let compress = 0;
+      let route = 'connector.entryHandler.entry';
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_REQUEST, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       id.should.equal(res.id);
       Message.TYPE_REQUEST.should.equal(res.type);
@@ -55,14 +55,14 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding empty route', function() {
-      var id = 256;
-      var compress = 0;
-      var route = '';
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_REQUEST, compress,
+      let id = 256;
+      let compress = 0;
+      let route = '';
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_REQUEST, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       id.should.equal(res.id);
       Message.TYPE_REQUEST.should.equal(res.type);
@@ -73,15 +73,15 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding null route', function() {
-      var n = Math.floor(10000*Math.random());
-      var id = 128 * n;
-      var compress = 0;
-      var route = null;
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_REQUEST, compress,
+      let n = Math.floor(10000*Math.random());
+      let id = 128 * n;
+      let compress = 0;
+      let route = null;
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_REQUEST, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       id.should.equal(res.id);
       Message.TYPE_REQUEST.should.equal(res.type);
@@ -92,14 +92,14 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding compress route', function() {
-      var id = 256;
-      var compress = 1;
-      var route = 3;
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_REQUEST, compress,
+      let id = 256;
+      let compress = 1;
+      let route = 3;
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_REQUEST, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
 
       id.should.equal(res.id);
@@ -111,14 +111,14 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding mutil-bytes id', function() {
-      var id = Math.pow(2, 30);
-      var compress = 1;
-      var route = 3;
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_REQUEST, compress,
+      let id = Math.pow(2, 30);
+      let compress = 1;
+      let route = 3;
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_REQUEST, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       id.should.equal(res.id);
       Message.TYPE_REQUEST.should.equal(res.type);
@@ -130,13 +130,13 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding notify', function() {
-      var compress = 0;
-      var route = 'connector.entryHandler.entry';
-      var msg = 'hello world~';
-      var buf = Message.encode(0, Message.TYPE_NOTIFY, compress,
+      let compress = 0;
+      let route = 'connector.entryHandler.entry';
+      let msg = 'hello world~';
+      let buf = Message.encode(0, Message.TYPE_NOTIFY, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       res.id.should.equal(0);
       Message.TYPE_NOTIFY.should.equal(res.type);
@@ -147,13 +147,13 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding response', function() {
-      var id = 1;
-      var compress = 0;
-      var msg = 'hello world~';
-      var buf = Message.encode(id, Message.TYPE_RESPONSE, compress,
+      let id = 1;
+      let compress = 0;
+      let msg = 'hello world~';
+      let buf = Message.encode(id, Message.TYPE_RESPONSE, compress,
                                null, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       id.should.equal(res.id);
       Message.TYPE_RESPONSE.should.equal(res.type);
@@ -164,13 +164,13 @@ describe('Pomelo protocol test', function() {
     });
 
     it('should be ok for encoding and decoding push', function() {
-      var compress = 0;
-      var route = 'connector.entryHandler.entry';
-      var msg = 'hello world~';
-      var buf = Message.encode(0, Message.TYPE_PUSH, compress,
+      let compress = 0;
+      let route = 'connector.entryHandler.entry';
+      let msg = 'hello world~';
+      let buf = Message.encode(0, Message.TYPE_PUSH, compress,
                                route, Protocol.strencode(msg));
       should.exist(buf);
-      var res = Message.decode(buf);
+      let res = Message.decode(buf);
       should.exist(res);
       res.id.should.equal(0);
       Message.TYPE_PUSH.should.equal(res.type);
